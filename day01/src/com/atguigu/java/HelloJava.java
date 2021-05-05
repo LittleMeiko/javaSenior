2fbdee21670ef38d08b7a0cf2924306203cba865
meiko_glb <yishengxin_glb@163.com>
2021-05-05 18:18:33 +0800

    HelloJava

---
+++ day01/src/com/atguigu/java/HelloJava.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+/**
+ * HelloJava
+ * @author Meiko
+ * @create 2021-05-05 18:01
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+    }
+}
--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.atguigu.java;
 
+import java.util.Arrays;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("HelloWorld.main");
     }
 }
