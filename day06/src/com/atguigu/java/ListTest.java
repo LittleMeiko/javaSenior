de8059bfd20af6eda6b9a3b92280d2f4c2950134
meiko_glb <yishengxin_glb@163.com>
2021-06-08 21:02:29 +0800

    ArrayList、LinkedList、Vector底层源码分析

--- day06/src/com/atguigu/java/ListTest.java
+++ day06/src/com/atguigu/java/ListTest.java
@@ -10,6 +10,27 @@ package com.atguigu.java;
  *         LinkedList(JDK1.2)：对于频繁的插入、删除操作，使用此类效率比ArrayList高；底层使用双向链表存储；
  *         Vector(JDK1.0)：List接口的古老实现类；线程安全的，效率低；
  *
+ * 1.ArrayList底层源码分析：
+ *    JDK7：ArrayList list = new ArrayList();//底层创建了一个长度为10的Object[]数组elementData
+ *          list.add(123);//elementData[0]=new Integer(123)
+ *          ...
+ *          list.add(789);//如果此次添加导致底层elementData数组容量不够则扩容。默认情况下扩容为原来的1.5倍，同时将原来数组
+ *          中的元素复制到新的数组中。
+ *    JDK8中的变化：
+ *          ArrayList list = new ArrayList();//底层Object[] elementData初始化为{},并没有创建长度为10的数组
+ *          list.add(123);//第一次调用add()时才创建了长度为10的数组，并将数据123添加到elementData[0]
+ *          ...
+ *          后续的添加与扩容与JDK7无异
+ *    结论：JDK8中延迟了数组的创建时间，节省了内存空间。
+ *
+ * 2.LinkedList底层源码分析：
+ *    LinkedList list = new LinkedList();//内部声明了一个类型为Node的first和last属性，默认值为null
+ *    list.add(123);//将123封装到Node中，创建了Node对象。
+ *
+ * 3.Vector底层源码分析：
+ *    在JDK7和JDK8中通过Vector()构造方法创建对象时，都创建了一个长度为10的Object[] elementData数组
+ *    在扩容方面，默认为扩容为原来数组长度的2倍
+ *
  *
  * @author Meiko
  * @date 2021-06-03
